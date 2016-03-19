6d378dda8ef4d5c9cb5899ea3a338e9ee3410120
jdevmiller <jdevmiller@gmail.com>
2016-03-19 19:07:57 -0400

    remvove log call from "catch" method

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -33,7 +33,7 @@ public class ForecastFragment extends Fragment {
     //Instance variables
     private String httpApiCall = "http://api.openweathermap.org/data/2.5/forecast/daily?zip=" + CITY_ZIP + "&mode=json&units=metric&cnt=" + Integer.toString(FORECAST_DAYS) + "&appid=" + API_KEY;
 
-    public URL url;
+    private URL url;
 
     public ForecastFragment() {
     }
