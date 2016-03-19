070f5d0579e74c04d92159190ede6b82ff9d464c
jdevmiller <jdevmiller@gmail.com>
2016-03-19 19:42:26 -0400

    refactor onOptionsItemSelected() method

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -59,11 +59,14 @@ public class ForecastFragment extends Fragment {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_refresh) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
