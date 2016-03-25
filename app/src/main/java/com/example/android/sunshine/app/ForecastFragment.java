2dd946439fd54bc13a628cddb03df7c3e5265deb
jdevmiller <jdevmiller@gmail.com>
2016-03-24 21:30:23 -0400

    separate buffer.append(line + "\n") into two buffer.append() arguments

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -162,7 +162,9 @@ public class ForecastFragment extends Fragment {
                     // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
                     // But it does make debugging a *lot* easier if you print out the completed
                     // buffer for debugging.
-                    buffer.append(line + "\n");
+                    buffer
+                            .append(line)
+                            .append("\n");
                 }
 
                 if (buffer.length() == 0) {
